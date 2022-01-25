bf597ae3efacc6b6a835e1db66ba198cba2a9317
Esteban Saldarriaga Alzate <estebansa@outlook.com>
2022-01-24 19:46:31 -0500

    Database Sharding: Reading

--- 5. Database sharding/demo/sharding-backend/index.js
+++ 5. Database sharding/demo/sharding-backend/index.js
@@ -1,9 +1,8 @@
 const { Client } = require('pg');
 const app = require('express')();
 const crypto = require('crypto');
-const ConsistentHash = require('consistent-hash');
-
-const hr = new ConsistentHash();
+const HashRing = require('hashring');
+const hr = new HashRing();
 
 hr.add('5433');
 hr.add('5434');
@@ -34,16 +33,23 @@ const clients = {
 }
 
 connect();
+
 async function connect() {
   await clients['5433'].connect();
   await clients['5434'].connect();
   await clients['5435'].connect();
 }
 
-app.get('/', (req, res) => {
-  res.send({
-    name: 'Esteban'
-  })
+app.get('/:urlId', async (req, res) => {
+  const urlId = req.params.urlId;
+  const server = hr.get(urlId);
+  const result = await clients[server].query('SELECT * FROM URL_TABLE WHERE url_id = $1', [urlId]);
+
+  if (result.rowCount > 0) {
+    res.send({ ...result.rows[0], server });
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 app.post('/', async (req, res) => {
